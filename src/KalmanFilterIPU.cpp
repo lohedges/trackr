dd5a6a2759870a0f5c43ee130dc2ec970cc23494
Lester Hedges <lester.hedges@gmail.com>
2021-06-15 11:44:06 +0100

    Sub-parallelise over hardware threads on IPU tiles.

--- src/KalmanFilterIPU.cpp
+++ src/KalmanFilterIPU.cpp
@@ -162,134 +162,159 @@ void KalmanFilterIPU::setupGraphProgram()
     // Create a compute set.
     poplar::ComputeSet computeSet = this->graph.addComputeSet("computeSet");
 
+    // Tally counter for hits that have been processed so far.
+    int hit_tally = 0;
+
     // Loop over the number of IPU tiles.
     for (int i=0; i<this->num_tiles; ++i)
     {
-        // Create the tile index string.
-        std::string tile_id = std::to_string(i);
-
-        // Initalise state of system: { x, tan(theta), y, tan(phi) }
-        // Use {x, 0, y, 0} column vectors for the initial hits. The matrix is
-        // packed as hits along the columns and planes along the rows. (Every
-        // fourth row is the start of a new plane.)
-        MatrixRowMajorXf p0 = MatrixRowMajorXf::Zero(4*this->num_planes, this->hits_per_tile);
-        for (int j=0; j<this->num_planes; ++j)
+        // Create 6 worker threads per tile. (Each tile has 6 hardware threads.)
+        for (int j=0; j<6; ++j)
         {
-            p0.row(j*4 + 0) = this->hits_plane[j].col(0).transpose().segment(i*this->hits_per_tile, this->hits_per_tile);
-            p0.row(j*4 + 2) = this->hits_plane[j].col(1).transpose().segment(i*this->hits_per_tile, this->hits_per_tile);
+            // Work out the number of hits per thread.
+            int hits_per_thread = int(this->hits_per_tile / 6);
+
+            // If this is the first thread, then batch any extra tracks from
+            // the remainder of the division.
+            if (j == 0)
+            {
+                int remainder = int(this->hits_per_tile - 6*hits_per_thread);
+                hits_per_thread += remainder;
+            }
+
+            // Only continue if there are hits to process.
+            if (hits_per_thread > 0)
+            {
+                // Initalise state of system: { x, tan(theta), y, tan(phi) }
+                // Use {x, 0, y, 0} column vectors for the initial hits. The matrix is
+                // packed as hits along the columns and planes along the rows. (Every
+                // fourth row is the start of a new plane.)
+                MatrixRowMajorXf p0 = MatrixRowMajorXf::Zero(4*this->num_planes, hits_per_thread);
+                for (int k=0; k<this->num_planes; ++k)
+                {
+                    p0.row(k*4 + 0) = this->hits_plane[k].col(0).transpose().segment(hit_tally, hits_per_thread);
+                    p0.row(k*4 + 2) = this->hits_plane[k].col(1).transpose().segment(hit_tally, hits_per_thread);
+                }
+
+                // Update the hit tally.
+                hit_tally += hits_per_thread;
+
+                // Number of rows and columns for packed matrices.
+                auto nRows = ulong(4*this->num_planes);
+                auto nCols = ulong(hits_per_thread);
+
+                // Create the vertex identification string.
+                std::string vertex_id = std::to_string(i) + std::to_string(j);
+
+                // Create the graph variables.
+                poplar::Tensor v_p = this->graph.addVariable(poplar::FLOAT, {4, nCols}, "p" + vertex_id);
+                poplar::Tensor v_m = this->graph.addVariable(poplar::FLOAT, {4, nCols}, "m" + vertex_id);
+                poplar::Tensor v_p0 = this->graph.addVariable(poplar::FLOAT, {nRows, nCols}, "p0" + vertex_id);
+                poplar::Tensor v_p_proj = this->graph.addVariable(poplar::FLOAT, {4, nCols}, "p_proj" + vertex_id);
+                poplar::Tensor v_p_projs = this->graph.addVariable(poplar::FLOAT, {nRows, nCols}, "p_projs" + vertex_id);
+                poplar::Tensor v_p_filt = this->graph.addVariable(poplar::FLOAT, {4, nCols}, "p_filt" + vertex_id);
+                poplar::Tensor v_p_filts = this->graph.addVariable(poplar::FLOAT, {nRows, nCols}, "p_filts" + vertex_id);
+                poplar::Tensor v_p_smooth = this->graph.addVariable(poplar::FLOAT, {4, nCols}, "p_smooth" + vertex_id);
+                poplar::Tensor v_p_smooths = this->graph.addVariable(poplar::FLOAT, {nRows, nCols}, "p_smooths" + vertex_id);
+                poplar::Tensor v_F = this->graph.addVariable(poplar::FLOAT, {4, 4}, "F" + vertex_id);
+                poplar::Tensor v_C = this->graph.addVariable(poplar::FLOAT, {4, 4}, "C0" + vertex_id);
+                poplar::Tensor v_C0 = this->graph.addVariable(poplar::FLOAT, {4, 4}, "C0" + vertex_id);
+                poplar::Tensor v_C_proj = this->graph.addVariable(poplar::FLOAT, {4, 4}, "C_proj" + vertex_id);
+                poplar::Tensor v_C_projs = this->graph.addVariable(poplar::FLOAT, {nRows, 4}, "C_projs" + vertex_id);
+                poplar::Tensor v_C_filt = this->graph.addVariable(poplar::FLOAT, {4, 4}, "C_filt" + vertex_id);
+                poplar::Tensor v_C_filts = this->graph.addVariable(poplar::FLOAT, {nRows, 4}, "C_filts" + vertex_id);
+                poplar::Tensor v_FT = this->graph.addVariable(poplar::FLOAT, {4, 4}, "FT" + vertex_id);
+                poplar::Tensor v_HTG = this->graph.addVariable(poplar::FLOAT, {4, 4}, "HTG" + vertex_id);
+                poplar::Tensor v_HTGH = this->graph.addVariable(poplar::FLOAT, {4, 4}, "HTGH" + vertex_id);
+                poplar::Tensor v_tmp_4x4_0 = this->graph.addVariable(poplar::FLOAT, {4, 4}, "tmp_4x4_0" + vertex_id);
+                poplar::Tensor v_tmp_4x4_1 = this->graph.addVariable(poplar::FLOAT, {4, 4}, "tmp_4x4_1" + vertex_id);
+                poplar::Tensor v_tmp_4x4_2 = this->graph.addVariable(poplar::FLOAT, {4, 4}, "tmp_4x4_2" + vertex_id);
+                poplar::Tensor v_tmp_4xN_0 = this->graph.addVariable(poplar::FLOAT, {4, nCols}, "tmp_4xN_0" + vertex_id);
+                poplar::Tensor v_tmp_4xN_1 = this->graph.addVariable(poplar::FLOAT, {4, nCols}, "tmp_4xN_1" + vertex_id);
+                this->graph.setTileMapping(v_p, i);
+                this->graph.setTileMapping(v_m, i);
+                this->graph.setTileMapping(v_p0, i);
+                this->graph.setTileMapping(v_p_proj, i);
+                this->graph.setTileMapping(v_p_projs, i);
+                this->graph.setTileMapping(v_p_filt, i);
+                this->graph.setTileMapping(v_p_filts, i);
+                this->graph.setTileMapping(v_p_smooth, i);
+                this->graph.setTileMapping(v_p_smooths, i);
+                this->graph.setTileMapping(v_F, i);
+                this->graph.setTileMapping(v_C, i);
+                this->graph.setTileMapping(v_C0, i);
+                this->graph.setTileMapping(v_C_proj, i);
+                this->graph.setTileMapping(v_C_projs, i);
+                this->graph.setTileMapping(v_C_filt, i);
+                this->graph.setTileMapping(v_C_filts, i);
+                this->graph.setTileMapping(v_FT, i);
+                this->graph.setTileMapping(v_HTG, i);
+                this->graph.setTileMapping(v_HTGH, i);
+                this->graph.setTileMapping(v_tmp_4x4_0, i);
+                this->graph.setTileMapping(v_tmp_4x4_1, i);
+                this->graph.setTileMapping(v_tmp_4x4_2, i);
+                this->graph.setTileMapping(v_tmp_4xN_0, i);
+                this->graph.setTileMapping(v_tmp_4xN_1, i);
+
+                // Create the graph constants.
+                poplar::Tensor c_p0 = this->graph.addConstant<float>(poplar::FLOAT, {nRows, nCols}, p0.data());
+                poplar::Tensor c_F = this->graph.addConstant<float>(poplar::FLOAT, {4, 4}, F.data());
+                poplar::Tensor c_C0 = this->graph.addConstant<float>(poplar::FLOAT, {4, 4}, C0.data());
+                poplar::Tensor c_FT = this->graph.addConstant<float>(poplar::FLOAT, {4, 4}, FT.data());
+                poplar::Tensor c_HTG = this->graph.addConstant<float>(poplar::FLOAT, {4, 4}, HTG.eval().data());
+                poplar::Tensor c_HTGH = this->graph.addConstant<float>(poplar::FLOAT, {4, 4}, HTGH.eval().data());
+                this->graph.setTileMapping(c_p0, i);
+                this->graph.setTileMapping(c_F, i);
+                this->graph.setTileMapping(c_C0, i);
+                this->graph.setTileMapping(c_FT, i);
+                this->graph.setTileMapping(c_HTG, i);
+                this->graph.setTileMapping(c_HTGH, i);
+
+                // Copy the inital data.
+                this->prog.add(poplar::program::Copy(c_p0, v_p0));
+                this->prog.add(poplar::program::Copy(c_F, v_F));
+                this->prog.add(poplar::program::Copy(c_C0, v_C0));
+                this->prog.add(poplar::program::Copy(c_FT, v_FT));
+                this->prog.add(poplar::program::Copy(c_HTG, v_HTG));
+                this->prog.add(poplar::program::Copy(c_HTGH, v_HTGH));
+
+                // Add a vertex to the compute set.
+                poplar::VertexRef vtx = this->graph.addVertex(computeSet, "KalmanFilter");
+
+                // Connect graph variables to the vertex.
+                this->graph.connect(vtx["p"], v_p);
+                this->graph.connect(vtx["m"], v_m);
+                this->graph.connect(vtx["p0"], v_p0);
+                this->graph.connect(vtx["p_proj"], v_p_proj);
+                this->graph.connect(vtx["p_projs"], v_p_projs);
+                this->graph.connect(vtx["p_filt"], v_p_filt);
+                this->graph.connect(vtx["p_filts"], v_p_filts);
+                this->graph.connect(vtx["p_smooth"], v_p_smooth);
+                this->graph.connect(vtx["p_smooths"], v_p_smooths);
+                this->graph.connect(vtx["F"], v_F);
+                this->graph.connect(vtx["FT"], v_FT);
+                this->graph.connect(vtx["C"], v_C);
+                this->graph.connect(vtx["C0"], v_C0);
+                this->graph.connect(vtx["C_proj"], v_C_proj);
+                this->graph.connect(vtx["C_projs"], v_C_projs);
+                this->graph.connect(vtx["C_filt"], v_C_filt);
+                this->graph.connect(vtx["C_filts"], v_C_filts);
+                this->graph.connect(vtx["HTG"], v_HTG);
+                this->graph.connect(vtx["HTGH"], v_HTGH);
+                this->graph.connect(vtx["tmp_4x4_0"], v_tmp_4x4_0);
+                this->graph.connect(vtx["tmp_4x4_1"], v_tmp_4x4_1);
+                this->graph.connect(vtx["tmp_4x4_2"], v_tmp_4x4_2);
+                this->graph.connect(vtx["tmp_4xN_0"], v_tmp_4xN_0);
+                this->graph.connect(vtx["tmp_4xN_1"], v_tmp_4xN_1);
+
+                // Map the vertex to a tile.
+                this->graph.setTileMapping(vtx, i);
+
+                // Create a read to the smoothed tracks for testing.
+                if (i == 0 && j == 0)
+                    this->graph.createHostRead("p_smooths", v_p_smooths);
+            }
         }
-
-        // Number of rows and columns for packed matrices.
-        auto nRows = ulong(4*this->num_planes);
-        auto nCols = ulong(this->hits_per_tile);
-
-        // Create the graph variables.
-        poplar::Tensor v_p = this->graph.addVariable(poplar::FLOAT, {4, nCols}, "p" + tile_id);
-        poplar::Tensor v_m = this->graph.addVariable(poplar::FLOAT, {4, nCols}, "m" + tile_id);
-        poplar::Tensor v_p0 = this->graph.addVariable(poplar::FLOAT, {nRows, nCols}, "p0" + tile_id);
-        poplar::Tensor v_p_proj = this->graph.addVariable(poplar::FLOAT, {4, nCols}, "p_proj" + tile_id);
-        poplar::Tensor v_p_projs = this->graph.addVariable(poplar::FLOAT, {nRows, nCols}, "p_projs" + tile_id);
-        poplar::Tensor v_p_filt = this->graph.addVariable(poplar::FLOAT, {4, nCols}, "p_filt" + tile_id);
-        poplar::Tensor v_p_filts = this->graph.addVariable(poplar::FLOAT, {nRows, nCols}, "p_filts" + tile_id);
-        poplar::Tensor v_p_smooth = this->graph.addVariable(poplar::FLOAT, {4, nCols}, "p_smooth" + tile_id);
-        poplar::Tensor v_p_smooths = this->graph.addVariable(poplar::FLOAT, {nRows, nCols}, "p_smooths" + tile_id);
-        poplar::Tensor v_F = this->graph.addVariable(poplar::FLOAT, {4, 4}, "F" + tile_id);
-        poplar::Tensor v_C = this->graph.addVariable(poplar::FLOAT, {4, 4}, "C0" + tile_id);
-        poplar::Tensor v_C0 = this->graph.addVariable(poplar::FLOAT, {4, 4}, "C0" + tile_id);
-        poplar::Tensor v_C_proj = this->graph.addVariable(poplar::FLOAT, {4, 4}, "C_proj" + tile_id);
-        poplar::Tensor v_C_projs = this->graph.addVariable(poplar::FLOAT, {nRows, 4}, "C_projs" + tile_id);
-        poplar::Tensor v_C_filt = this->graph.addVariable(poplar::FLOAT, {4, 4}, "C_filt" + tile_id);
-        poplar::Tensor v_C_filts = this->graph.addVariable(poplar::FLOAT, {nRows, 4}, "C_filts" + tile_id);
-        poplar::Tensor v_FT = this->graph.addVariable(poplar::FLOAT, {4, 4}, "FT" + tile_id);
-        poplar::Tensor v_HTG = this->graph.addVariable(poplar::FLOAT, {4, 4}, "HTG" + tile_id);
-        poplar::Tensor v_HTGH = this->graph.addVariable(poplar::FLOAT, {4, 4}, "HTGH" + tile_id);
-        poplar::Tensor v_tmp_4x4_0 = this->graph.addVariable(poplar::FLOAT, {4, 4}, "tmp_4x4_0" + tile_id);
-        poplar::Tensor v_tmp_4x4_1 = this->graph.addVariable(poplar::FLOAT, {4, 4}, "tmp_4x4_1" + tile_id);
-        poplar::Tensor v_tmp_4x4_2 = this->graph.addVariable(poplar::FLOAT, {4, 4}, "tmp_4x4_2" + tile_id);
-        poplar::Tensor v_tmp_4xN_0 = this->graph.addVariable(poplar::FLOAT, {4, nCols}, "tmp_4xN_0" + tile_id);
-        poplar::Tensor v_tmp_4xN_1 = this->graph.addVariable(poplar::FLOAT, {4, nCols}, "tmp_4xN_1" + tile_id);
-        this->graph.setTileMapping(v_p, i);
-        this->graph.setTileMapping(v_m, i);
-        this->graph.setTileMapping(v_p0, i);
-        this->graph.setTileMapping(v_p_proj, i);
-        this->graph.setTileMapping(v_p_projs, i);
-        this->graph.setTileMapping(v_p_filt, i);
-        this->graph.setTileMapping(v_p_filts, i);
-        this->graph.setTileMapping(v_p_smooth, i);
-        this->graph.setTileMapping(v_p_smooths, i);
-        this->graph.setTileMapping(v_F, i);
-        this->graph.setTileMapping(v_C, i);
-        this->graph.setTileMapping(v_C0, i);
-        this->graph.setTileMapping(v_C_proj, i);
-        this->graph.setTileMapping(v_C_projs, i);
-        this->graph.setTileMapping(v_C_filt, i);
-        this->graph.setTileMapping(v_C_filts, i);
-        this->graph.setTileMapping(v_FT, i);
-        this->graph.setTileMapping(v_HTG, i);
-        this->graph.setTileMapping(v_HTGH, i);
-        this->graph.setTileMapping(v_tmp_4x4_0, i);
-        this->graph.setTileMapping(v_tmp_4x4_1, i);
-        this->graph.setTileMapping(v_tmp_4x4_2, i);
-        this->graph.setTileMapping(v_tmp_4xN_0, i);
-        this->graph.setTileMapping(v_tmp_4xN_1, i);
-
-        // Create the graph constants.
-        poplar::Tensor c_p0 = this->graph.addConstant<float>(poplar::FLOAT, {nRows, nCols}, p0.data());
-        poplar::Tensor c_F = this->graph.addConstant<float>(poplar::FLOAT, {4, 4}, F.data());
-        poplar::Tensor c_C0 = this->graph.addConstant<float>(poplar::FLOAT, {4, 4}, C0.data());
-        poplar::Tensor c_FT = this->graph.addConstant<float>(poplar::FLOAT, {4, 4}, FT.data());
-        poplar::Tensor c_HTG = this->graph.addConstant<float>(poplar::FLOAT, {4, 4}, HTG.eval().data());
-        poplar::Tensor c_HTGH = this->graph.addConstant<float>(poplar::FLOAT, {4, 4}, HTGH.eval().data());
-        this->graph.setTileMapping(c_p0, i);
-        this->graph.setTileMapping(c_F, i);
-        this->graph.setTileMapping(c_C0, i);
-        this->graph.setTileMapping(c_FT, i);
-        this->graph.setTileMapping(c_HTG, i);
-        this->graph.setTileMapping(c_HTGH, i);
-
-        // Copy the inital data.
-        this->prog.add(poplar::program::Copy(c_p0, v_p0));
-        this->prog.add(poplar::program::Copy(c_F, v_F));
-        this->prog.add(poplar::program::Copy(c_C0, v_C0));
-        this->prog.add(poplar::program::Copy(c_FT, v_FT));
-        this->prog.add(poplar::program::Copy(c_HTG, v_HTG));
-        this->prog.add(poplar::program::Copy(c_HTGH, v_HTGH));
-
-        // Add a vertex to the compute set.
-        poplar::VertexRef vtx = this->graph.addVertex(computeSet, "KalmanFilter");
-
-        // Connect graph variables to the vertex.
-        this->graph.connect(vtx["p"], v_p);
-        this->graph.connect(vtx["m"], v_m);
-        this->graph.connect(vtx["p0"], v_p0);
-        this->graph.connect(vtx["p_proj"], v_p_proj);
-        this->graph.connect(vtx["p_projs"], v_p_projs);
-        this->graph.connect(vtx["p_filt"], v_p_filt);
-        this->graph.connect(vtx["p_filts"], v_p_filts);
-        this->graph.connect(vtx["p_smooth"], v_p_smooth);
-        this->graph.connect(vtx["p_smooths"], v_p_smooths);
-        this->graph.connect(vtx["F"], v_F);
-        this->graph.connect(vtx["FT"], v_FT);
-        this->graph.connect(vtx["C"], v_C);
-        this->graph.connect(vtx["C0"], v_C0);
-        this->graph.connect(vtx["C_proj"], v_C_proj);
-        this->graph.connect(vtx["C_projs"], v_C_projs);
-        this->graph.connect(vtx["C_filt"], v_C_filt);
-        this->graph.connect(vtx["C_filts"], v_C_filts);
-        this->graph.connect(vtx["HTG"], v_HTG);
-        this->graph.connect(vtx["HTGH"], v_HTGH);
-        this->graph.connect(vtx["tmp_4x4_0"], v_tmp_4x4_0);
-        this->graph.connect(vtx["tmp_4x4_1"], v_tmp_4x4_1);
-        this->graph.connect(vtx["tmp_4x4_2"], v_tmp_4x4_2);
-        this->graph.connect(vtx["tmp_4xN_0"], v_tmp_4xN_0);
-        this->graph.connect(vtx["tmp_4xN_1"], v_tmp_4xN_1);
-
-        // Map the vertex to a tile.
-        this->graph.setTileMapping(vtx, i);
-
-        // Create a read to the smoothed tracks for testing.
-        if (i == 0)
-            this->graph.createHostRead("p_smooths", v_p_smooths);
     }
 
     // Add the compute set to the program.
