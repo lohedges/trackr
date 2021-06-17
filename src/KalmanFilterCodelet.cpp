6559b97013070d06a2db9e27c3476b4fa537fcec
Lester Hedges <lester.hedges@gmail.com>
2021-06-17 09:52:32 +0100

    Simplify codelet using function overloading and typedefs.

--- src/KalmanFilterCodelet.cpp
+++ src/KalmanFilterCodelet.cpp
@@ -17,52 +17,34 @@
 
 #include <poplar/Vertex.hpp>
 
+// Handy typedefs.
+using InputFloat = poplar::Vector<poplar::Input<poplar::Vector<float>>>;
+using InOutFloat = poplar::Vector<poplar::InOut<poplar::Vector<float>>>;
+
 // Unoptimised helper functions prototypes.
 
 // Copy a tensor 'in' into 'out'. The offsets allow us to specify the
 // starting row in the input and output tensor to begin reading and
 // writing.
-void copy_IO(poplar::Vector<poplar::Input<poplar::Vector<float>>> &in,
-             poplar::Vector<poplar::InOut<poplar::Vector<float>>> &out,
-             int offset_in,
-             int offset_out);
-void copy_OO(poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in,
-             poplar::Vector<poplar::InOut<poplar::Vector<float>>> &out,
-             int offset_in,
-             int offset_out);
+void copy(InputFloat &in, InOutFloat &out, int offset_in, int offset_out);
+void copy(InOutFloat &in, InOutFloat &out, int offset_in, int offset_out);
 
 // Sum tensors 'in0' and 'in1', placing the result in 'out'.
-void sum_IO(poplar::Vector<poplar::Input<poplar::Vector<float>>> &in0,
-            poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in1,
-            poplar::Vector<poplar::InOut<poplar::Vector<float>>> &out);
-void sum_OO(poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in0,
-            poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in1,
-            poplar::Vector<poplar::InOut<poplar::Vector<float>>> &out);
+void sum(InputFloat &in0, InOutFloat &in1, InOutFloat &out);
+void sum(InOutFloat &in0, InOutFloat &in1, InOutFloat &out);
 
 // Subtract tensors 'in0' and 'in1', placing the result in 'out'.
-void sub_IO(poplar::Vector<poplar::Input<poplar::Vector<float>>> &in0,
-            poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in1,
-            poplar::Vector<poplar::InOut<poplar::Vector<float>>> &out);
-void sub_OO(poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in0,
-            poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in1,
-            poplar::Vector<poplar::InOut<poplar::Vector<float>>> &out);
+void sub(InputFloat &in0, InOutFloat &in1, InOutFloat &out);
+void sub(InOutFloat &in0, InOutFloat &in1, InOutFloat &out);
 
 // Multiply matrices 'in0' and 'in1', placing the result in 'out'.
-void mul_IO(poplar::Vector<poplar::Input<poplar::Vector<float>>> &in0,
-            poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in1,
-            poplar::Vector<poplar::InOut<poplar::Vector<float>>> &out);
-void mul_OI(poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in0,
-            poplar::Vector<poplar::Input<poplar::Vector<float>>> &in1,
-            poplar::Vector<poplar::InOut<poplar::Vector<float>>> &out);
-void mul_OO(poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in0,
-            poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in1,
-            poplar::Vector<poplar::InOut<poplar::Vector<float>>> &out);
+void mul(InputFloat &in0, InOutFloat &in1, InOutFloat &out);
+void mul(InOutFloat &in0, InputFloat &in1, InOutFloat &out);
+void mul(InOutFloat &in0, InOutFloat &in1, InOutFloat &out);
 
 // Compute the inverse of matrix 'in', placing the result in 'out'.
-void inverse_I(poplar::Vector<poplar::Input<poplar::Vector<float>>> &in,
-               poplar::Vector<poplar::InOut<poplar::Vector<float>>> &out);
-void inverse_O(poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in,
-               poplar::Vector<poplar::InOut<poplar::Vector<float>>> &out);
+void inverse(InputFloat &in, InOutFloat &out);
+void inverse(InOutFloat &in, InOutFloat &out);
 
 // Kalman Filter Codelet.
 
@@ -70,32 +52,32 @@ class KalmanFilter : public poplar::Vertex
 {
 public:
     // Input fields. (constant)
-    poplar::Vector<poplar::Input<poplar::Vector<float>>> p0;
-    poplar::Vector<poplar::Input<poplar::Vector<float>>> F;
-    poplar::Vector<poplar::Input<poplar::Vector<float>>> FT;
-    poplar::Vector<poplar::Input<poplar::Vector<float>>> C0;
-    poplar::Vector<poplar::Input<poplar::Vector<float>>> HTG;
-    poplar::Vector<poplar::Input<poplar::Vector<float>>> HTGH;
+    InputFloat p0;
+    InputFloat F;
+    InputFloat FT;
+    InputFloat C0;
+    InputFloat HTG;
+    InputFloat HTGH;
 
     // InOut fields. (read/writeable)
-    poplar::Vector<poplar::InOut<poplar::Vector<float>>> p;
-    poplar::Vector<poplar::InOut<poplar::Vector<float>>> m;
-    poplar::Vector<poplar::InOut<poplar::Vector<float>>> p_proj;
-    poplar::Vector<poplar::InOut<poplar::Vector<float>>> p_projs;
-    poplar::Vector<poplar::InOut<poplar::Vector<float>>> p_filt;
-    poplar::Vector<poplar::InOut<poplar::Vector<float>>> p_filts;
-    poplar::Vector<poplar::InOut<poplar::Vector<float>>> p_smooth;
-    poplar::Vector<poplar::InOut<poplar::Vector<float>>> p_smooths;
-    poplar::Vector<poplar::InOut<poplar::Vector<float>>> C;
-    poplar::Vector<poplar::InOut<poplar::Vector<float>>> C_proj;
-    poplar::Vector<poplar::InOut<poplar::Vector<float>>> C_projs;
-    poplar::Vector<poplar::InOut<poplar::Vector<float>>> C_filt;
-    poplar::Vector<poplar::InOut<poplar::Vector<float>>> C_filts;
-    poplar::Vector<poplar::InOut<poplar::Vector<float>>> tmp_4x4_0;
-    poplar::Vector<poplar::InOut<poplar::Vector<float>>> tmp_4x4_1;
-    poplar::Vector<poplar::InOut<poplar::Vector<float>>> tmp_4x4_2;
-    poplar::Vector<poplar::InOut<poplar::Vector<float>>> tmp_4xN_0;
-    poplar::Vector<poplar::InOut<poplar::Vector<float>>> tmp_4xN_1;
+    InOutFloat p;
+    InOutFloat m;
+    InOutFloat p_proj;
+    InOutFloat p_projs;
+    InOutFloat p_filt;
+    InOutFloat p_filts;
+    InOutFloat p_smooth;
+    InOutFloat p_smooths;
+    InOutFloat C;
+    InOutFloat C_proj;
+    InOutFloat C_projs;
+    InOutFloat C_filt;
+    InOutFloat C_filts;
+    InOutFloat tmp_4x4_0;
+    InOutFloat tmp_4x4_1;
+    InOutFloat tmp_4x4_2;
+    InOutFloat tmp_4xN_0;
+    InOutFloat tmp_4xN_1;
 
     // Overloaded compute function.
     bool compute()
@@ -105,69 +87,69 @@ public:
         int num_planes = p0.size() / 4;
 
         // Copy the state vector for the first plane into p.
-        copy_IO(p0, p, 0, 0);
+        copy(p0, p, 0, 0);
 
         // Initialise the covariance matrix.
-        copy_IO(C0, C, 0, 0);
+        copy(C0, C, 0, 0);
 
         // Step forward through the planes to linearly project the state
         // and correct (filter) using the measured hits and covariance.
         for (int i=0; i<num_planes; ++i)
         {
             // p_proj = F * p
-            mul_IO(F, p, p_proj);
-            copy_OO(p_proj, p_projs, 0, i*4);
+            mul(F, p, p_proj);
+            copy(p_proj, p_projs, 0, i*4);
 
             // C_proj = F * C * FT
-            mul_IO(F, C, tmp_4x4_0);
-            mul_OI(tmp_4x4_0, FT, C_proj);
-            copy_OO(C_proj, C_projs, 0, i*4);
+            mul(F, C, tmp_4x4_0);
+            mul(tmp_4x4_0, FT, C_proj);
+            copy(C_proj, C_projs, 0, i*4);
 
             // C_filt = (C_proj.inverse() + HTGH).inverse()
-            inverse_O(C_proj, tmp_4x4_0);
-            sum_IO(HTGH, tmp_4x4_0, C_proj);
-            inverse_O(C_proj, C_filt);
-            copy_OO(C_filt, C_filts, 0, i*4);
+            inverse(C_proj, tmp_4x4_0);
+            sum(HTGH, tmp_4x4_0, C_proj);
+            inverse(C_proj, C_filt);
+            copy(C_filt, C_filts, 0, i*4);
 
             // tmp = (C_proj.inverse() * p_proj) + (HTG * m)
-            copy_IO(p0, m, i*4, 0);
-            mul_OO(tmp_4x4_0, p_proj, tmp_4xN_0);
-            mul_IO(HTG, m, tmp_4xN_1);
-            sum_OO(tmp_4xN_0, tmp_4xN_1, tmp_4xN_1);
+            copy(p0, m, i*4, 0);
+            mul(tmp_4x4_0, p_proj, tmp_4xN_0);
+            mul(HTG, m, tmp_4xN_1);
+            sum(tmp_4xN_0, tmp_4xN_1, tmp_4xN_1);
 
             // p_filt = C_filt * tmp
-            mul_OO(C_filt, tmp_4xN_1, p_filt);
-            copy_OO(p_filt, p_filts, 0, i*4);
+            mul(C_filt, tmp_4xN_1, p_filt);
+            copy(p_filt, p_filts, 0, i*4);
 
             // p = p_filt
-            copy_OO(p_filt, p, 0, 0);
+            copy(p_filt, p, 0, 0);
 
             // C = C_filt
-            copy_OO(C_filt, C, 0, 0);
+            copy(C_filt, C, 0, 0);
         }
 
         // Initialise the smmoothed hits for the final plane.
-        copy_OO(p_filt, p_smooth, 0, 0);
-        copy_OO(p_smooth, p_smooths, 0, (num_planes-1)*4);
+        copy(p_filt, p_smooth, 0, 0);
+        copy(p_smooth, p_smooths, 0, (num_planes-1)*4);
 
         // Now propagate state and covariance updates backwards through the
         // states to find the globally optimum configuration.
         for (int i=num_planes-2; i>=0; --i)
         {
             // A = C_filts[i] * FT * C_projs[i+1].inverse()
-            copy_OO(C_filts, C_filt, i*4, 0);
-            copy_OO(C_projs, C_proj, (i+1)*4, 0);
-            inverse_O(C_proj, tmp_4x4_1);
-            mul_OI(C_filt, FT, tmp_4x4_0);
-            mul_OO(tmp_4x4_0, tmp_4x4_1, tmp_4x4_2);
+            copy(C_filts, C_filt, i*4, 0);
+            copy(C_projs, C_proj, (i+1)*4, 0);
+            inverse(C_proj, tmp_4x4_1);
+            mul(C_filt, FT, tmp_4x4_0);
+            mul(tmp_4x4_0, tmp_4x4_1, tmp_4x4_2);
 
             // p_smooth = p_filts[i] + A*(p_smoothed[i+1] - p_projs[i+1]);
-            copy_OO(p_filts, p_filt, i*4, 0);
-            copy_OO(p_projs, p_proj, (i+1)*4, 0);
-            sub_OO(p_smooth, p_proj, p_smooth);
-            mul_OO(tmp_4x4_2, p_smooth, tmp_4xN_0);
-            sum_OO(p_filt, tmp_4xN_0, p_smooth);
-            copy_OO(p_smooth, p_smooths, 0, i*4);
+            copy(p_filts, p_filt, i*4, 0);
+            copy(p_projs, p_proj, (i+1)*4, 0);
+            sub(p_smooth, p_proj, p_smooth);
+            mul(tmp_4x4_2, p_smooth, tmp_4xN_0);
+            sum(p_filt, tmp_4xN_0, p_smooth);
+            copy(p_smooth, p_smooths, 0, i*4);
         }
 
         return true;
@@ -177,10 +159,7 @@ public:
 
 // Unoptimised helper functions declarations.
 
-void copy_IO(poplar::Vector<poplar::Input<poplar::Vector<float>>> &in,
-             poplar::Vector<poplar::InOut<poplar::Vector<float>>> &out,
-             int offset_in,
-             int offset_out)
+void copy(InputFloat &in, InOutFloat &out, int offset_in, int offset_out)
 {
     for (int i=0; i<4; ++i)
     {
@@ -191,10 +170,7 @@ void copy_IO(poplar::Vector<poplar::Input<poplar::Vector<float>>> &in,
     }
 }
 
-void copy_OO(poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in,
-             poplar::Vector<poplar::InOut<poplar::Vector<float>>> &out,
-             int offset_in,
-             int offset_out)
+void copy(InOutFloat &in, InOutFloat &out, int offset_in, int offset_out)
 {
     for (int i=0; i<4; ++i)
     {
@@ -205,9 +181,7 @@ void copy_OO(poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in,
     }
 }
 
-void sum_IO(poplar::Vector<poplar::Input<poplar::Vector<float>>> &in0,
-            poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in1,
-            poplar::Vector<poplar::InOut<poplar::Vector<float>>> &out)
+void sum(InputFloat &in0, InOutFloat &in1, InOutFloat &out)
 {
     for (int i=0; i<4; ++i)
     {
@@ -218,9 +192,7 @@ void sum_IO(poplar::Vector<poplar::Input<poplar::Vector<float>>> &in0,
     }
 }
 
-void sum_OO(poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in0,
-            poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in1,
-            poplar::Vector<poplar::InOut<poplar::Vector<float>>> &out)
+void sum(InOutFloat &in0, InOutFloat &in1, InOutFloat &out)
 {
     for (int i=0; i<4; ++i)
     {
@@ -231,9 +203,7 @@ void sum_OO(poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in0,
     }
 }
 
-void sub_IO(poplar::Vector<poplar::Input<poplar::Vector<float>>> &in0,
-            poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in1,
-            poplar::Vector<poplar::InOut<poplar::Vector<float>>> &out)
+void sub(InputFloat &in0, InOutFloat &in1, InOutFloat &out)
 {
     for (int i=0; i<4; ++i)
     {
@@ -244,9 +214,7 @@ void sub_IO(poplar::Vector<poplar::Input<poplar::Vector<float>>> &in0,
     }
 }
 
-void sub_OO(poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in0,
-            poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in1,
-            poplar::Vector<poplar::InOut<poplar::Vector<float>>> &out)
+void sub(InOutFloat &in0, InOutFloat &in1, InOutFloat &out)
 {
     for (int i=0; i<4; ++i)
     {
@@ -257,9 +225,7 @@ void sub_OO(poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in0,
     }
 }
 
-void mul_IO(poplar::Vector<poplar::Input<poplar::Vector<float>>> &in0,
-            poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in1,
-            poplar::Vector<poplar::InOut<poplar::Vector<float>>> &out)
+void mul(InputFloat &in0, InOutFloat &in1, InOutFloat &out)
 {
     for (int i=0; i<4; ++i)
     {
@@ -274,9 +240,7 @@ void mul_IO(poplar::Vector<poplar::Input<poplar::Vector<float>>> &in0,
     }
 }
 
-void mul_OI(poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in0,
-            poplar::Vector<poplar::Input<poplar::Vector<float>>> &in1,
-            poplar::Vector<poplar::InOut<poplar::Vector<float>>> &out)
+void mul(InOutFloat &in0, InputFloat &in1, InOutFloat &out)
 {
     for (int i=0; i<4; ++i)
     {
@@ -291,9 +255,7 @@ void mul_OI(poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in0,
     }
 }
 
-void mul_OO(poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in0,
-            poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in1,
-            poplar::Vector<poplar::InOut<poplar::Vector<float>>> &out)
+void mul(InOutFloat &in0, InOutFloat &in1, InOutFloat &out)
 {
     for (int i=0; i<4; ++i)
     {
@@ -308,8 +270,7 @@ void mul_OO(poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in0,
     }
 }
 
-void inverse_I(poplar::Vector<poplar::Input<poplar::Vector<float>>> &in,
-               poplar::Vector<poplar::InOut<poplar::Vector<float>>> &out)
+void inverse(InputFloat &in, InOutFloat &out)
 {
     float A2323 = in[2][2] * in[3][3] - in[2][3] * in[3][2];
     float A1323 = in[2][1] * in[3][3] - in[2][3] * in[3][1];
@@ -355,8 +316,7 @@ void inverse_I(poplar::Vector<poplar::Input<poplar::Vector<float>>> &in,
     out[3][3] = det *   ( in[0][0] * A1212 - in[0][1] * A0212 + in[0][2] * A0112 );
 }
 
-void inverse_O(poplar::Vector<poplar::InOut<poplar::Vector<float>>> &in,
-               poplar::Vector<poplar::InOut<poplar::Vector<float>>> &out)
+void inverse(InOutFloat &in, InOutFloat &out)
 {
     float A2323 = in[2][2] * in[3][3] - in[2][3] * in[3][2];
     float A1323 = in[2][1] * in[3][3] - in[2][3] * in[3][1];
